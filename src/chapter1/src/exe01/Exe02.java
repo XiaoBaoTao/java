b8e33bfe287f5e3ff41a8b50fbef55129ff79415
xiaobaotao1990 <xiaobaotao1990@gmail.com>
2022-07-09 09:35:25 +0800

    exe02

---
+++ src/chapter1/src/exe01/Exe02.java
@@ -0,0 +1,23 @@
+package exe01;
+
+public class Exe02 {
+    public static void main(String[] args) {
+        /*
+        请编写程序，分别定义8种基本数据类型的变量，并打印变量的值。
+         */
+        byte a = 1;
+        short b = 1;
+        char c = 'a';
+        int d = 12;
+        long e = 12;
+        float f = 12.0f;
+        double g = 123.34;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+}
