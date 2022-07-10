14fba914299fb4457a906f1060d48c2a74bede4c
xiaobaotao1990 <xiaobaotao1990@gmail.com>
2022-07-10 22:15:44 +0800

    数组和集合demo

---
+++ src/chapter4/src/main/java/Exercise/Exe01.java
@@ -0,0 +1,24 @@
+package Exercise;
+
+import Student.Student;
+
+import java.util.Scanner;
+
+public class Exe01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 数组的长度是固定的，集合的长度是可变的
+        Student[] students = new Student[3];
+        for (int i = 0; i < 3; i++) {
+            System.out.print("please input name: ");
+            String name = sc.next();
+            System.out.print("please input age: ");
+            int age = sc.nextInt();
+            Student student = new Student(name, age);
+            students[i] = student;
+        }
+        for (Student student : students) {
+            System.out.println(student.toString());
+        }
+    }
+}
